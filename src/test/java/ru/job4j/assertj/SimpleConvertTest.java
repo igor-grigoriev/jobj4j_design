ae46c75015c2ad41b6cb9d76853fc6657f1a0f35
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-09-13 15:53:51 +0300

    Утверждения с коллекциями [#504885]

---
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.assertj;
+
+import org.assertj.core.data.Index;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SimpleConvertTest {
+    @Test
+    void checkArray() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        String[] array = simpleConvert.toArray("first", "second", "three", "four", "five");
+        assertThat(array).hasSize(5)
+                .contains("second")
+                .contains("first", Index.atIndex(0))
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("first", Index.atIndex(1));
+    }
+
+    @Test
+    void checkList() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        List<String> list = simpleConvert.toList("first", "second", "three", "four", "five");
+        assertThat(list).hasSize(5)
+                .allMatch(e -> !e.contains("a"))
+                .filteredOn(e -> e.length() < 5).last().isEqualTo("five");
+    }
+
+    @Test
+    void checkSet() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Set<String> set = simpleConvert.toSet("first", "second", "three", "four", "five");
+        assertThat(set).hasSize(5)
+                .startsWith("four", "three")
+                .allSatisfy(e -> assertThat(e.length()).isGreaterThan(1))
+                .element(2).isNotNull().isEqualTo("five");
+    }
+
+    @Test
+    void checkMap() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Map<String, Integer> map = simpleConvert.toMap("first", "second", "three", "four", "five");
+        assertThat(map).hasSize(5)
+                .containsKeys("first", "second", "three", "four", "five")
+                .containsValues(0, 1, 2, 3, 4)
+                .containsEntry("first", 0);
+    }
+}
