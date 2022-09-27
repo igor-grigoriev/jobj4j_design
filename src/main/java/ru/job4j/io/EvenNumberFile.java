c8d7ecc1c39b5ca0b15e4c9772df230b38bd35cd
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-09-27 14:12:35 +0300

    0.2. FileInputStream [#4898] fix

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
@@ -14,7 +15,7 @@ public class EvenNumberFile {
                 int number = Integer.parseInt(s);
                 System.out.println((number % 2 == 0) ? number + " четное" : number + " нечетное");
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
