4d521497008d1410fab9e19f60b3e7d0dbda573b
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-10-04 16:29:46 +0300

    7. Scanner [#504791] fix

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,15 +1,18 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class CSVReader {
-    public static void handle(ArgsName argsName) throws Exception {
+    public static void handle(ArgsName argsName) {
         validateArgs(argsName);
         List<Integer> indexes = new ArrayList<>();
         String[] columns = argsName.get("filter").split(",");
         try (Scanner scan = new Scanner(new FileReader(argsName.get("path")));
-             PrintWriter pw = new PrintWriter(new FileWriter(argsName.get("out")))) {
+             PrintWriter pw = ("stdout".equals(argsName.get("out")))
+                     ? new PrintWriter(System.out)
+                     : new PrintWriter(new FileWriter(argsName.get("out")))) {
             scan.useDelimiter(System.lineSeparator());
             if (scan.hasNext()) {
                 StringJoiner joiner = new StringJoiner(argsName.get("delimiter"));
@@ -42,8 +45,14 @@ public class CSVReader {
         if (!args.get("path").endsWith(".csv")) {
             throw new IllegalArgumentException(String.format("Invalid argument number 1 %s", args.get("path")));
         }
-        args.get("delimiter");
-        if (!args.get("out").endsWith(".csv")) {
+        File file = Paths.get(args.get("path")).toFile();
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!args.get("delimiter").matches("[\\W{1}]")) {
+            throw new IllegalArgumentException(String.format("Invalid argument number 2 %s", args.get("delimiter")));
+        }
+        if (!"stdout".equals(args.get("out")) && !args.get("out").endsWith(".csv")) {
             throw new IllegalArgumentException(String.format("Invalid argument number 3 %s", args.get("out")));
         }
         args.get("filter");
