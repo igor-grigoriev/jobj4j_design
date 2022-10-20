71af52cd36b7474de57059348ef3b3ad7a1eee88
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-10-20 17:04:24 +0300

    1. Inner join [#373617] fix

--- sql/inner_join.sql
+++ sql/inner_join.sql
@@ -1,3 +1,28 @@
-select user.name, role.name from role join users on role_id = u.id;
-select u.name, r.name from role as r join users as u on role_id = u.id;
-select u.name as Пользователь, r.name as Роль from role as r join users as u on role_id = u.id;
+create table role (
+	id serial primary key,
+	name varchar(255)
+);
+
+create table users (
+	id serial primary key,
+	name varchar(255),
+	role_id int references role(id)
+);
+
+insert into role(name) values('role');
+insert into users(name,role_id) values('user',1);
+
+select user.name, role.name
+from role join users
+on role_id = u.id;
+
+select u.name, r.name
+from role as r
+join users as u
+on role_id = u.id;
+
+select u.name as Пользователь,
+r.name as Роль
+from role as r
+join users as u
+on role_id = u.id;
