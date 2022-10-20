6bdffd6880574737117aec4a2c7f89f1ba1bf6f2
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-10-20 13:00:31 +0300

    1. Схема прав пользователи и роли. [#1093]

---
+++ sql/insert.sql
@@ -0,0 +1,9 @@
+﻿insert into role(name) values('role');
+insert into state(name) values('state');
+insert into rules(name) values('rule');
+insert into category(name) values('category');
+insert into users(name,role_id) values('user',1);
+insert into item(name,user_id,category_id,state_id) values ('item',1,1,1);
+insert into attachs(name,item_id) values('attach',1);
+insert into comments(name,item_id) values('comment',1);
+insert into role_rules(role_id,rule_id) values(1,1);
