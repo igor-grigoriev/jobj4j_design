48b8ec90c183e6a7fd6b48d4d97296c3e9de2f72
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-10-24 18:39:15 +0300

    2. Группировка и агрегатные функции [#373618] fix

--- sql/devices_people.sql
+++ sql/devices_people.sql
@@ -1,14 +1,46 @@
+create table devices(
+    id serial primary key,
+    name varchar(255),
+    price float
+);
+
+create table people(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table devices_people(
+    id serial primary key,
+    device_id int references devices(id),
+    people_id int references people(id)
+);
+
+insert into devices(name,price) values('device1',1);
+insert into devices(name,price) values('device2',2);
+insert into devices(name,price) values('device3',3);
+insert into devices(name,price) values('device4',4);
+insert into people(name) values('people1');
+insert into people(name) values('people2');
+insert into devices_people(device_id,people_id) values(1,1);
+insert into devices_people(device_id,people_id) values(2,1);
+insert into devices_people(device_id,people_id) values(3,2);
+insert into devices_people(device_id,people_id) values(4,2);
+
 select avg(price) from devices;
 
-select dp.people_id, avg(d.price) 
+select p.name, avg(d.price) 
 from devices_people as dp
-join devices as d 
+join devices as d
 on dp.device_id  = d.id
-group by dp.people_id;
+join people as p
+on dp.people_id = p.id
+group by p.name;
 
-select dp.people_id, avg(d.price) 
+select p.name, avg(d.price) 
 from devices_people as dp
-join devices as d 
+join devices as d
 on dp.device_id  = d.id
-group by dp.people_id
+join people as p
+on dp.people_id = p.id
+group by p.name;
 having avg(d.price) > 5000;
