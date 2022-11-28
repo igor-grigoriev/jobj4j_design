74138beea0e3cee73b691cfb99dceb972bd52b7a
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-11-28 16:04:19 +0300

    4.4. Транзакции. Работа с транзакциями в PostgreSQL [#504813] fix

--- sql/transactions.sql
+++ sql/transactions.sql
@@ -6,3 +6,27 @@ create table students(
 
 insert into students(name, age) values('Иван', 19);
 insert into students(name, age) values('Петр', 21);
+
+begin transaction;
+
+select * from students;
+
+update students set age = 20;
+
+savepoint first;
+
+insert into students(name,age) values('Ivan',33);
+
+savepoint second;
+
+select * from students;
+
+rollback to first;
+
+select * from students;
+
+rollback to second;
+
+select * from students;
+
+rollback;
