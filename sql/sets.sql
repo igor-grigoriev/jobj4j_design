f0a49cf01e9d4d499b0014663efa2a118346e81a
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-12-13 11:47:05 +0300

    10. Операторы множества в SQL [#504896]

--- sql/sets.sql
+++ sql/sets.sql
@@ -32,6 +32,10 @@ select title from book
 except
 select name from movie;
 
-select name from movie
+(select name from movie
+except
+select title from book)
 union
-select title from book;
+(select title from book
+except
+select name from movie);
