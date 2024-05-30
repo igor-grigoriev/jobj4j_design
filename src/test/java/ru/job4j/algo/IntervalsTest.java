fb24c32bc5e9ef7f211f7448cf2b3c8cf4390382
igor-grigoriev <grigoriev.i.v@mail.ru>
2024-05-30 15:20:18 +0300

    Сортировки [#505155] fix

--- src/test/java/ru/job4j/algo/IntervalsTest.java
+++ src/test/java/ru/job4j/algo/IntervalsTest.java
@@ -8,8 +8,8 @@ public class IntervalsTest {
     @Test
     public void whenIntervalsHaveOverlapsThenMergeOverlappingIntervals() {
         Intervals intervalMerger = new Intervals();
-        int[][] intervals = new int[][] { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } };
-        int[][] expected = new int[][] { { 1, 6 }, { 8, 10 }, { 15, 18 } };
+        int[][] intervals = new int[][] {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] expected = new int[][] {{1, 6}, {8, 10}, {15, 18}};
         int[][] result = intervalMerger.merge(intervals);
         assertThat(result).isDeepEqualTo(expected);
     }
@@ -17,8 +17,8 @@ public class IntervalsTest {
     @Test
     public void whenIntervalsTouchAtEdgesThenMergeIntervals() {
         Intervals intervalMerger = new Intervals();
-        int[][] intervals = new int[][] { { 1, 4 }, { 4, 5 } };
-        int[][] expected = new int[][] { { 1, 5 } };
+        int[][] intervals = new int[][] {{1, 4}, {4, 5}};
+        int[][] expected = new int[][] {{1, 5}};
         int[][] result = intervalMerger.merge(intervals);
         assertThat(result).isDeepEqualTo(expected);
     }
@@ -26,8 +26,8 @@ public class IntervalsTest {
     @Test
     public void whenIntervalsAreNonOverlappingThenReturnSameIntervals() {
         Intervals intervalMerger = new Intervals();
-        int[][] intervals = new int[][] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
-        int[][] expected = new int[][] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+        int[][] intervals = new int[][] {{1, 2}, {3, 4}, {5, 6}};
+        int[][] expected = new int[][] {{1, 2}, {3, 4}, {5, 6}};
         int[][] result = intervalMerger.merge(intervals);
         assertThat(result).isDeepEqualTo(expected);
     }
@@ -35,8 +35,8 @@ public class IntervalsTest {
     @Test
     public void whenIntervalsFullyOverlapThenMergeIntoOneInterval() {
         Intervals intervalMerger = new Intervals();
-        int[][] intervals = new int[][] { { 1, 5 }, { 2, 6 }, { 3, 7 } };
-        int[][] expected = new int[][] { { 1, 7 } };
+        int[][] intervals = new int[][] {{1, 5}, {2, 6}, {3, 7}};
+        int[][] expected = new int[][] {{1, 7}};
         int[][] result = intervalMerger.merge(intervals);
         assertThat(result).isDeepEqualTo(expected);
     }
@@ -44,8 +44,8 @@ public class IntervalsTest {
     @Test
     public void whenIntervalsAreMixedThenMergeOverlappingAndKeepNonOverlapping() {
         Intervals intervalMerger = new Intervals();
-        int[][] intervals = new int[][] { { 1, 4 }, { 0, 2 }, { 3, 5 } };
-        int[][] expected = new int[][] { { 0, 5 } };
+        int[][] intervals = new int[][] {{1, 4}, {0, 2}, {3, 5}};
+        int[][] expected = new int[][] {{0, 5}};
         int[][] result = intervalMerger.merge(intervals);
         assertThat(result).isDeepEqualTo(expected);
     }
