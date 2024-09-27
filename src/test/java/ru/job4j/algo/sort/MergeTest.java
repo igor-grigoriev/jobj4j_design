1ef35f46a6ed6d4b38b7b8a038d3617f5f9e0d4e
igor-grigoriev <grigoriev.i.v@mail.ru>
2024-09-27 08:43:08 +0300

    Сортировка слиянием [#504997] fix

--- src/test/java/ru/job4j/algo/sort/MergeTest.java
+++ src/test/java/ru/job4j/algo/sort/MergeTest.java
@@ -5,8 +5,8 @@ import static org.assertj.core.api.Assertions.*;
 
 class MergeTest {
     @Test
-    void whenSortedThenOk(){
-        int[] array = { 10, 4, 6, 4, 8, -13, 2, 3 };
+    void whenSortedThenOk() {
+        int[] array = {10, 4, 6, 4, 8, -13, 2, 3};
         assertThat(Merge.mergesort(array)).containsExactly(-13, 2, 3, 4, 4, 6, 8, 10);
     }
 }
